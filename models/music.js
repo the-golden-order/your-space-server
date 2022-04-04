610427294022bee94373208530a49b17ab442089
Thomas Basham <Bashamtg@gmail.com>
2022-04-04 16:01:40 -0700

    created seed.js, added seed data to mongodb

---
+++ models/music.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const mongoose = require('mongoose');
+
+const{ Schema } = mongoose;
+
+const musicSchema = new Schema({
+
+    artistName : {type: String},
+    trackName : {type: String},
+    artWork : {type: String},
+    genre : {type: String},
+    note : {type: String},
+    email : {type: String}
+
+  })
+
+const SearchedSong = mongoose.model('music', musicSchema);
+
+module.exports = SearchedSong;
--- server.js
+++ server.js
@@ -3,28 +3,27 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// mongoose.connect(process.env.DB_URL);
+mongoose.connect(process.env.DB_URL);
 
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error: '));
-// db.once('open', function (){
-//   console.log('Mongoose is connected');
-// })
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', function (){
+  console.log('Mongoose is connected');
+})
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 // REQUIRE MODULES
 const getItunes = require("./modules/Itunes");
 
+const PORT = process.env.PORT || 3002;
 // GET ROUTES FROM MODULES
 app.get("/Itunes", getItunes);
-
-
-const PORT = process.env.PORT || 3002;
 
 app.get('/', (req, res) => {
   res.send('welcome to your space');
