562f6995618ef605623ce3c1b0296a51d2ca55ee
Thomas Basham <Bashamtg@gmail.com>
2022-04-04 14:56:11 -0700

    added itunes module and get route on server.js

--- server.js
+++ server.js
@@ -3,19 +3,26 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DB_URL);
+// mongoose.connect(process.env.DB_URL);
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error: '));
-db.once('open', function (){
-  console.log('Mongoose is connected');
-})
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error: '));
+// db.once('open', function (){
+//   console.log('Mongoose is connected');
+// })
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// REQUIRE MODULES
+const getItunes = require("./modules/Itunes");
+
+// GET ROUTES FROM MODULES
+app.get("/Itunes", getItunes);
+
 
 const PORT = process.env.PORT || 3002;
 
@@ -35,4 +42,4 @@ app.use((error, req, res)=> {
   res.status(500).send(error.message);
 })
 
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+app.listen(PORT, () => console.log(`Server is serving on port ${PORT}`));
