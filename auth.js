416ba78ed0eb99db463b8270b609dcb7674a99f0
Marcus Hartwig <marcusdnhartwig@gmail.com>
2022-04-06 11:25:48 -0700

    auth0 is working sin error for user

--- auth.js
+++ auth.js
@@ -1,29 +1,30 @@
 'use strict';
 
-// jwt is json web token
+// jwt - json web token - also pronounced "jot"
 const jwt = require('jsonwebtoken');
 
-// jwks is the json web key set
+// jwks - json web key set - also pronunced "ja-wicks"
 const jwksClient = require('jwks-rsa');
 
-// the jwks uri is in Auth0 account page -> Advanced Settings -> Endpoints -> 0Auth -> JSON Web Key Set 
+// the jwks uri come Auth0 account page -> advanced settings -> Endpoints -> 0auth -> JSON Web Key Set
 const client = jwksClient({
   jwksUri: process.env.JWKS_URI
 });
 
-//I need a getKey function from the Json web token to make things work
 
+// I need a getKet function from jsonwebtoken to make things work
+// from:  https://www.npmjs.com/package/jsonwebtoken - search for "auth0"
 function getKey(header, callback){
-  client.getSigningKey(header.kid, function(err, jey){
+  client.getSigningKey(header.kid, function(err, key) {
     var signingKey = key.publicKey || key.rsaPublicKey;
     callback(null, signingKey);
   });
 }
 
-// we are writing this function to verify the user on our route 
-//this is how we do it
+// we are writing this function to verify the user on our route
+// this is how we do it!
 function verifyUser (req, errorFirstOrUserCallbackFunction){
-  try{
+  try {
     const token = req.headers.authorization.split(' ')[1];
     console.log(token);
     jwt.verify(token, getKey, {}, errorFirstOrUserCallbackFunction);
@@ -31,6 +32,5 @@ function verifyUser (req, errorFirstOrUserCallbackFunction){
     errorFirstOrUserCallbackFunction('not authorized');
   }
 }
-
-module.exports = verifyUser;
+ module.exports = verifyUser;
 
--- modules/getMusic.js
+++ modules/getMusic.js
@@ -1,5 +1,6 @@
 "use strict";
-// const verifyUser = require('../auth');
+const verifyUser = require('../auth');
+
 
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DB_URL);
@@ -7,24 +8,24 @@ const musicSchema = require("../models/music");
 
 
 async function getMusic(req, res) {
-  // verifyUser(req, async (err) => {
-  //   if (err) {
-  //     console.error(err);
-  //     res.send('invalid token');
-   // } else {
+  verifyUser(req, async (err, user) => {
+    if (err) {
+      console.error(err);
+      res.send('invalid token');
+   } else {
       try {
-        // const musicQuery = {};
-        // if (user.email){
-        //   musicQuery.email = user.email;
-        // }
+        const musicQuery = {};
+        if (user.email){
+          musicQuery.email = user.email;
+        }
         let results = await musicSchema.find();
         res.status(200).send(results);
       } catch (error) {
         console.log(error.message);
         res.status(500).send(error.message);
       }
-  //   }
-  // });
+    }
+  });
 }
 
 module.exports = getMusic;
