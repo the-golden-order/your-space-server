82a17301648ffec5b9d02d2101165028255e53f3
Marcus Hartwig <marcusdnhartwig@gmail.com>
2022-04-06 17:15:34 -0700

    dont add

--- modules/addMusic.js
+++ modules/addMusic.js
@@ -3,27 +3,27 @@
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DB_URL);
 const musicSchema = require('../models/music');
-// const verifyUser = require('../auth');
+const verifyUser = require('../auth');
 
 async function addMusic(req, res) {
-  // verifyUser(req, async (err, user) => {
-  //   if (err) {
-  //     console.error(err);
-  //     res.send('invalid token');
-  //  } else {
+  verifyUser(req, async (err, user) => {
+    if (err) {
+      console.error(err);
+      res.send('invalid token');
+   } else {
       try {
-        // const musicQuery = {};
-        // if(user.email){
-        //   musicQuery.email = user.email;
-        // }
+        const musicQuery = {};
+        if(user.email){
+          musicQuery.email = user.email;
+        }
         let addedSong = await musicSchema.create(req.body);
         res.status(201).send(addedSong);
       } catch (error) {
         console.log(error.message);
         res.status(500).send(error.message);
       }
-  //   }
-  // });
+    }
+  });
 }
 
 module.exports = addMusic;
